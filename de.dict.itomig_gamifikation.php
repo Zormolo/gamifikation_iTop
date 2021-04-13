71e64dbd0eea7d2e9fb39b66fcd221a3469e1962
max renner <max.renner@itomig.de>
2021-04-13 15:14:13 +0200

    Initalisation of the projekt

---
+++ de.dict.itomig_gamifikation.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+	'Class:Playerinfo' => 'Spielerinformation',
+	'Class:Playerinfo+' => 'Alle Informationen über einen Spieler!',
+	'Class:Playerinfo/Attribute:nickname' => 'Nickname',
+	'Class:Playerinfo/Attribute:nickname+' => 'Dein Spielername',
+	'Class:Playerinfo/Attribute:player' => 'Spieler',
+	'Class:Playerinfo/Attribute:exp' => 'EXP',
+	'Class:Playerinfo/Attribute:exp+' => 'Menge der Erfahrungspunkte',
+	'Class:Playerinfo/Attribute:lv' => 'Level',
+	'Class:Playerinfo/Attribute:lv+' => 'Level des Spielers',
+	'Class:Playerinfo/Attribute:lv_progress' => 'Fortschritt',
+	'Class:Playerinfo/Attribute:lv_progress+' => 'Fortschritt der EXP für das nächste Level!',
+	'Class:Playerinfo/Attribute:score' => 'Score',
+	'Class:Playerinfo/Attribute:score+' => 'Scorepunkte deines Accounts',
+	'Class:Playerinfo/Attribute:gender' => 'Geschlecht',
+	'Class:Playerinfo/Attribute:gender+' => 'Geschlecht des Spielers',
+	'Class:Playerinfo/Attribute:gender/Value:male' => 'männlich',
+	'Class:Playerinfo/Attribute:gender/Value:female' => 'weiblich',
+	'Class:Playerinfo/Attribute:icon' => 'Icon',
+	'Class:Playerinfo/Attribute:icon+' => 'Icon des Spielers',
+
+
+	'Menu:Gamifikation' => 'Gamifikation',
+	'Menu:Playerinfo:Player' => 'Neuen Spieler erstellen',
+	'Menu:Playerinfo:Highscore' => 'Highscore-Liste',
+));
+?>
---
+++ en.dict.itomig_gamifikation.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:Playerinfo' => 'Player information',
+	'Class:Playerinfo+' => 'All information of a player!',
+	'Class:Playerinfo/Attribute:nickname' => 'Nickname',
+	'Class:Playerinfo/Attribute:nickname+' => 'Your playername',
+	'Class:Playerinfo/Attribute:player' => 'Player',
+	'Class:Playerinfo/Attribute:exp' => 'EXP',
+	'Class:Playerinfo/Attribute:exp+' => 'Amount of experience points',
+	'Class:Playerinfo/Attribute:lv' => 'Level',
+	'Class:Playerinfo/Attribute:lv+' => 'Level of the player',
+	'Class:Playerinfo/Attribute:lv_progress' => 'Progress',
+	'Class:Playerinfo/Attribute:lv_progress+' => 'Progress of EXP to the next level',
+	'Class:Playerinfo/Attribute:score' => 'Score',
+	'Class:Playerinfo/Attribute:score+' => 'Scorepoints of your account',
+	'Class:Playerinfo/Attribute:gender' => 'Gender',
+	'Class:Playerinfo/Attribute:gender+' => 'Gender of the player',
+	'Class:Playerinfo/Attribute:gender/Value:male' => 'male',
+	'Class:Playerinfo/Attribute:gender/Value:female' => 'female',
+	'Class:Playerinfo/Attribute:icon' => 'Icon',
+	'Class:Playerinfo/Attribute:icon+' => 'Icon of the player',
+
+
+
+	'Menu:Gamifikation' => 'Gamifikation',
+	'Menu:Playerinfo:Player' => 'New Player',
+	'Menu:Playerinfo:Highscore' => 'Highscore-Board',
+
+));
+?>
